eed97d7ba67d80c1258bcb4068e5533f15523400
cfalexandre <alefonc321@hotmail.com>
2024-09-03 11:35:22 -0300

    Adicionando o ex2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+qtd_max = int(input("digite a quantidade maxima de peças"))
+print(qtd_max)
+
+qtd_min = int(input("digite a quantidade minima"))
+print(qtd_min)
+
+media = (qtd_max + qtd_min)/2
+print(f"a quantidade media é: {media}")
