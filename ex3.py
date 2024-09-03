5a70bdce7e6146efe94cc8fcc447cfb2afb0ad39
cfalexandre <alefonc321@hotmail.com>
2024-09-03 14:11:33 -0300

    Modificando o ex2

---
+++ ex3.py
@@ -0,0 +1,8 @@
+num1 = int(input("digite o primeiro número: "))
+num2 = int(input("digite o segundo número: "))
+
+soma = num1 + num2
+print(f"o resultado da soma é: {soma}")
+
+multi = num1 * soma
+print(f"o resultado final da multiplicação é: {multi}")
