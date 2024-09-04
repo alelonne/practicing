46225d4607a6a21ebb103e2bf1eee02effc43c7c
cfalexandre <alefonc321@hotmail.com>
2024-09-04 15:19:34 -0300

    ex6

---
+++ ex6.py
@@ -0,0 +1,10 @@
+nome_aluno = input("Digite o nome do aluno: ")
+rm_aluno = input("Digite o RM: ")
+nota1 = float(input("Digite a nota da primeira Ac: "))
+nota2 = float(input("Digite a nota da segunda Ac: "))
+nota3 = float(input("Digite a nota da terceira Ac: "))
+
+nota_final = nota1 + nota2 + nota3
+
+print(f"Nome: {nome_aluno} \nRM: {rm_aluno}")
+print(f"A nota final do aluno {nome_aluno} é: {nota_final}")
