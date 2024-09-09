9388cea3c51dd67fe7d50136a3efb308735715fa
cfalexandre <alefonc321@hotmail.com>
2024-09-09 09:31:57 -0300

    ex8

---
+++ ex8.py
@@ -0,0 +1,6 @@
+val_dol = float(input("Digite a cotação atual do Dólar americano: "))
+qtd_real = float(input("Digite a quantidade de reais que pretende converter: "))
+
+val_conv = qtd_real * val_dol
+
+print(f"O valor total da conversão é de: {val_conv}")
