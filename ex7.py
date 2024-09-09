aa34e51c28408dec51cfc13e666d6fef83c613a0
cfalexandre <alefonc321@hotmail.com>
2024-09-09 09:23:06 -0300

    ex7

---
+++ ex7.py
@@ -0,0 +1,8 @@
+cod_peca = input("Digite o código da peça: ")
+val_peca = float(input("Digite o valor da peça: "))
+qtd_peca = int(input("Digite a quantidade de peças: "))
+
+val_total = qtd_peca * val_peca
+
+print(f"O valor total da peça {cod_peca} é de {val_total}")
+
